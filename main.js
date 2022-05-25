d6c752467c6e43af362c7113046793db16040e29
Elise Gondange <elise@MacBook-Pro-de-Elise.local>
2022-05-25 12:22:34 +0200

    [GAME] add fruits generator

--- main.js
+++ main.js
@@ -7,16 +7,19 @@ var basketLeft =
 var basketBottom =
     parseInt(window.getComputedStyle(basket).getPropertyValue('bottom'));
 
+
+var score = 0;
+
 function moveBasketLeft() {
   if (basketLeft > 0) {
-    basketLeft -= 20;
+    basketLeft -= 30;
     basket.style.left = basketLeft + 'px';
   }
 }
 
 function moveBasketRight() {
   if (basketLeft < 620) {
-    basketLeft += 20;
+    basketLeft += 30;
     basket.style.left = basketLeft + 'px';
   }
 }
@@ -29,6 +32,36 @@ function control(e) {
     moveBasketRight();
   }
 }
+
+function generateFruits() {
+  var fruitBottom = 470;
+  var fruitLeft = Math.floor(Math.random() * 620);
+  var fruit = document.createElement('div');
+  fruit.setAttribute('class', 'fruit');
+  fruits.appendChild(fruit);
+
+  function fallDownFruit() {
+    if (fruitBottom < basketBottom + 50 && fruitBottom > basketBottom &&
+        fruitLeft > basketLeft - 30 && fruitLeft < basketLeft + 80) {
+      fruits.removeChild(fruit);
+      clearInterval(fallInterval);
+      score++;
+    }
+    if (fruitBottom < basketBottom) {
+      alert('Game Over ! Your score is : ' + score);
+      clearInterval(fallInterval);
+      clearTimeout(fruitTimeout);
+      location.reload();
+    }
+    fruitBottom -= 5;
+    fruit.style.bottom = fruitBottom + 'px';
+    fruit.style.left = fruitLeft + 'px';
+  }
+  var fallInterval = setInterval(fallDownFruit, 20);
+  var fruitTimeout = setTimeout(generateFruits, 2000);
+}
+
+generateFruits();
 
 
 document.addEventListener('keydown', control);
